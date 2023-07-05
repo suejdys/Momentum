a2c3c05de5a3a64f8b7fe8665549d2cf92750351
BOOK-GJUJSUS23D\suhye <1265asha38@gmail.com>
2023-07-05 13:25:24 +0900

    new git push

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+//const API_KEY = "c76f683ad71d12f45eb1ab16bdf4c320";
+const API_KEY = "2834387742b25d5393a21e88fee8246a";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOk(position){
+    const lat=position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+      });
+}
+function onGeoError(){
+    alert("위치를 찾을수 없습니다.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+
+
